4a95ac23dbe984131e4eadc2cf1ed178c7e9a438
NirvanaFan23146 <Poro5hin.d@yandex.ru>
2025-10-10 00:01:09 +0500

    add badgerdb

--- internal/store/badger/bager.go
+++ internal/store/badger/bager.go
@@ -1,1 +1,79 @@
 package badger
+
+
+import (
+	"log"
+	"github.com/dgraph-io/badger/v4"
+)
+
+
+type BadgerStore struct {
+	db   *badger.DB
+	path string
+}
+
+
+func New(path string) *BadgerStore {
+	db, err := badger.Open(badger.DefaultOptions(path))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &BadgerStore{db: db, path: path}
+}
+
+
+func (b *BadgerStore) Close() error {
+    return b.db.Close()
+}
+
+
+func (b *BadgerStore) Put(collection, key string, value []byte) error {
+	bytesKey := []byte(collection + "/" + key)
+
+	err := b.db.Update((func(txn *badger.Txn) error {
+		return txn.Set(bytesKey, value)
+	}))
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (b *BadgerStore) Get(collection, key string) ([]byte, error) {
+	bytesKey := []byte(collection + "/" + key)
+
+	var res []byte
+    err := b.db.View(func(txn *badger.Txn) error {
+        item, err := txn.Get(bytesKey)
+        if err != nil {
+            return err
+        }
+        res, err = item.ValueCopy(nil)
+        return err
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    return res, nil
+}
+
+
+func (b *BadgerStore) Delete(collection, key string) error {
+	bytesKey := []byte(collection + "/" + key)
+
+	err := b.db.Update((func(txn *badger.Txn) error {
+		return txn.Delete(bytesKey)
+	}))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- server/main.go
+++ server/main.go
@@ -1,14 +1,14 @@
 package main
 
+
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	// "github.com/AnotherOneDushnila/KV_Storage/internal/store"
 	httpapi "github.com/AnotherOneDushnila/KV_Storage/internal/api/http"
-	// badger "github.com/AnotherOneDushnila/KV_Storage/internal/store/badger"
+	badger "github.com/AnotherOneDushnila/KV_Storage/internal/store/badger"
 	"github.com/AnotherOneDushnila/KV_Storage/internal/store/inmemory"
 	"github.com/joho/godotenv"
 )
@@ -19,16 +19,24 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	backend := os.Getenv("STORAGE_BACKEND")
-	fmt.Println(backend)
 
+
+	if backend == "badger" {
+		s := badger.New("./tmp")
+		handler := httpapi.NewHandler(s)
+		http.HandleFunc("/ping", handler.Ping)
+		http.HandleFunc("/put", handler.PutHandler)
+		http.HandleFunc("/get", handler.GetHandler)
+		http.HandleFunc("/delete", handler.DeleteHandler)
+	} else {
+		s := memory.New()
+		handler := httpapi.NewHandler(s)
+		http.HandleFunc("/ping", handler.Ping)
+		http.HandleFunc("/put", handler.PutHandler)
+		http.HandleFunc("/get", handler.GetHandler)
+		http.HandleFunc("/delete", handler.DeleteHandler)
+	}
 	
-	s := memory.New()
-	handler := httpapi.NewHandler(s)
-	http.HandleFunc("/ping", handler.Ping)
-	http.HandleFunc("/put", handler.PutHandler)
-	http.HandleFunc("/get", handler.GetHandler)
-	http.HandleFunc("/delete", handler.DeleteHandler)
-
 
 	log.Println("Starting storage on :8080")
 	http.ListenAndServe(":8080", nil)
